6cbe976557294cbab78b775779616c7449b21e65
user140592 <user140592@eu201.local>
2018-12-23 20:40:40 +0000

    Initial Commit

---
+++ AoutoLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AoutoLayout
+//
+//  Created by user140592 on 12/23/18.
+//  Copyright © 2018 talspektor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
